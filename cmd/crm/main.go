13be2231cbf9938f18682324df9016bcbd7badbd
Jandir A. Cutabiala <me@jandir.co>
2023-12-18 00:04:40 -0300

    feat: creat all necessary routes

--- cmd/crm/main.go
+++ cmd/crm/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
 func main() {
+	r := chi.NewRouter()
 	println("Hello, World!")
+
+	r.Route("/api/v1/customers", func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			// Code goes here
+		})
+		r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
+			// Code goes here
+		})
+		r.Post("/", func(w http.ResponseWriter, r *http.Request) {
+			// Code goes here
+		})
+		r.Put("/{id}", func(w http.ResponseWriter, r *http.Request) {
+			// Code goes here
+		})
+		r.Delete("/{id}", func(w http.ResponseWriter, r *http.Request) {
+			// Code goes here
+		})
+	})
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jandiralceu/crm
 
 go 1.21.5
+
+require github.com/go-chi/chi/v5 v5.0.10 // indirect
