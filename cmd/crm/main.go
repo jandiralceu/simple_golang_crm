2dba41c0c9fb6296c39795147af6ad3b3ff12a2b
Jandir A. Cutabiala <me@jandir.co>
2023-12-24 02:08:16 -0300

    feat: map endpoints

--- cmd/crm/main.go
+++ cmd/crm/main.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jandiralceu/crm/internal/entities"
+	"github.com/jandiralceu/crm/internal/infra/database"
+	"github.com/jandiralceu/crm/internal/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 	"log"
 	"net/http"
 )
@@ -9,22 +15,26 @@ import (
 func main() {
 	r := chi.NewRouter()
 
+	db, err := gorm.Open(sqlite.Open("./cmd/crm/app.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	if err = db.AutoMigrate(&entities.Customer{}); err != nil {
+		panic("failed to migrate database")
+	}
+
+	customerHandler := handlers.NewCustomerHandler(database.CustomerDB(db))
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
 	r.Route("/api/v1/customers", func(r chi.Router) {
-		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-			// Code goes here
-		})
-		r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
-			// Code goes here
-		})
-		r.Post("/", func(w http.ResponseWriter, r *http.Request) {
-			// Code goes here
-		})
-		r.Put("/{id}", func(w http.ResponseWriter, r *http.Request) {
-			// Code goes here
-		})
-		r.Delete("/{id}", func(w http.ResponseWriter, r *http.Request) {
-			// Code goes here
-		})
+		r.Get("/", customerHandler.FindAll)
+		r.Get("/{id}", customerHandler.FindByID)
+		r.Post("/", customerHandler.Create)
+		r.Put("/{id}", customerHandler.Update)
+		r.Delete("/{id}", customerHandler.Delete)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", r))
